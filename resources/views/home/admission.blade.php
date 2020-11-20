e2ecba11b34e226d929fadf5490e83545207ba2e
Shantanu K <heyshantu13@gmail.com>
2020-11-20 17:10:51 +0530

    Update Admission Form

--- resources/views/home/admission.blade.php
+++ resources/views/home/admission.blade.php
@@ -112,23 +112,18 @@
                         </div>
 
                         <div class="form-group col-md-3">
-                            <label class="radio-inline">
-                                <input type="radio" name="gender" value="male" style="width: 16px;">Male
-                              </label>
-                              <label class="radio-inline" style="">
-                                <input type="radio" name="gender" value="female" style="width: 16px;">Female
-                              </label>
-                              @if ($errors->has('gender'))
-                              <span class="text-danger">{{ $errors->first('gender') }}</span>
-                          @endif
+                            <select class="form-control" id="sel1" name="gender">
+                                <option selected>Select Gender</option>
+                              <option value="male">Male</option>
+                              <option value="female">Female</option>
+                          
+                           
+                            </select>
                         </div>
 
                         
 
                      
-                        
-                        
-
                    
                         <div class="form-group col-md-12 col-sm-12">
                             <input name="address" type="address" placeholder="Enter Residence Address" rquired="" value="{{old('address')}}">
@@ -138,25 +133,64 @@
                         </div>
 
                         <div class="form-group col-md-4 col-sm-4">
-                            <input name="address" type="address" placeholder="Enter Residence Address" rquired="" value="{{old('address')}}">
-                            @if ($errors->has('address'))
-                  <span class="text-danger">{{ $errors->first('address') }}</span>
+                            <input name="city" type="text" placeholder="Enter City" rquired="" value="{{old('city')}}">
+                            @if ($errors->has('city'))
+                  <span class="text-danger">{{ $errors->first('city') }}</span>
               @endif
                         </div>
 
                         <div class="form-group col-md-4 col-sm-4">
-                            <input name="address" type="address" placeholder="Enter Residence Address" rquired="" value="{{old('address')}}">
-                            @if ($errors->has('address'))
-                  <span class="text-danger">{{ $errors->first('address') }}</span>
+                            <input name="tehsil" type="text" placeholder="Enter Tehsil/Taluka" rquired="" value="{{old('tehsil')}}">
+                            @if ($errors->has('tehsil'))
+                  <span class="text-danger">{{ $errors->first('tehsil') }}</span>
               @endif
                         </div>
 
                         <div class="form-group col-md-4 col-sm-4">
-                            <input name="address" type="address" placeholder="Enter Residence Address" rquired="" value="{{old('address')}}">
-                            @if ($errors->has('address'))
-                  <span class="text-danger">{{ $errors->first('address') }}</span>
+                            <input name="dist" type="text" placeholder="Enter District" rquired="" value="{{old('dist')}}">
+                            @if ($errors->has('dist'))
+                  <span class="text-danger">{{ $errors->first('dist') }}</span>
               @endif
                         </div>
+
+                        <div class="form-group col-md-6 col-sm-6">
+                            <input name="state" type="text" placeholder="Enter State" rquired="" value="{{old('state')}}">
+                            @if ($errors->has('state'))
+                  <span class="text-danger">{{ $errors->first('state') }}</span>
+              @endif
+                        </div>
+
+                        <div class="form-group col-md-6 col-sm-6">
+                            <input name="pincode" type="text" placeholder="Enter PinCode" rquired="" value="{{old('pincode')}}" maxlength="6">
+                            @if ($errors->has('pincode'))
+                  <span class="text-danger">{{ $errors->first('pincode') }}</span>
+              @endif
+                        </div>
+
+                        
+                        <div class="form-group col-md-4 col-sm-4">
+                            <input name="studentmobile" type="text" placeholder="Enter Student Mob No" rquired="" value="{{Auth::user()->mobile}}" readonly>
+                            @if ($errors->has('studentmobile'))
+                  <span class="text-danger">{{ $errors->first('studentmobile') }}</span>
+              @endif
+                        </div>
+
+                        <div class="form-group col-md-4 col-sm-4">
+                            <input name="parentmobile" type="text" placeholder="Enter Parent Mob No" rquired="" value="{{old('parentmobile')}}" readonly maxlength="10">
+                            @if ($errors->has('parentmobile'))
+                  <span class="text-danger">{{ $errors->first('parentmobile') }}</span>
+              @endif
+                        </div>
+
+                        <div class="form-group col-md-4 col-sm-4">
+                            <input name="aadharno" type="text" placeholder="Enter Aadhar No" rquired="" value="{{old('aadharno')}}" readonly maxlength="15">
+                            @if ($errors->has('aadharno'))
+                  <span class="text-danger">{{ $errors->first('aadharno') }}</span>
+              @endif
+                        </div>
+
+
+                        
                         
 
 
