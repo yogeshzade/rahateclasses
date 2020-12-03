64fa5db55f50d0fff44a0816c28b265495113dd7
Shantanu K <heyshantu13@gmail.com>
2020-12-03 14:23:44 +0530

    Update

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -39,7 +39,7 @@
    
    <section class="google-map">
         
-          <div class="mapouter" style="margin-left: 20px;"><div class="gmap_canvas"><iframe height="500"  id="gmap_canvas" src="https://maps.google.com/maps?q=Rahate%20Classes&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div><style>.mapouter{position:relative;text-align:right;height:500px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div>
+          <div class="mapouter" style="margin-left: 20px;"><div class="gmap_canvas"><iframe height="500" width="800px" id="gmap_canvas" src="https://maps.google.com/maps?q=Rahate%20Classes&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div><style>.mapouter{position:relative;text-align:right;height:500px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div>
        
         <div class="container">
             <div class="contact-detail">
