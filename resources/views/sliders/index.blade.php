66f87072b2db73ce0d24d1e3b36aef1e4cabcb1d
Shantanu Kulkarni <heyshantu13@gmail.com>
2021-03-09 19:47:30 +0530

    fix url in slider

--- resources/views/sliders/index.blade.php
+++ resources/views/sliders/index.blade.php
@@ -10,12 +10,12 @@
     <div class="col-md-12">
         <nav aria-label="breadcrumb" class="float-right mt-1">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="/">Dashboard</a></li>
-                <li class="breadcrumb-item"><a href="">Employees</a></li>
+                <li class="breadcrumb-item"><a href="/">Slider</a></li>
+                <li class="breadcrumb-item"><a href="">Lists</a></li>
                 <li class="breadcrumb-item active" aria-current="page">Manage</li>
             </ol>
         </nav>
-        <h4 class="mb-1 mt-0">Courses List</h4>
+        <h4 class="mb-1 mt-0">Slider List</h4>
     </div>
 </div>
 @endsection
@@ -45,7 +45,7 @@ $data = [
         <div class="card">
             <div class="card-body">
                 <h4 class="header-title mt-0 mb-1">
-                <a href="{{route('course.create')}}"> <button type="button" class="btn btn-success btn-sm pull-right">Create New </button> </a>
+                <a href="{{route('slider.add')}}"> <button type="button" class="btn btn-success btn-sm pull-right">Create New </button> </a>
                 </h4>
                <!--  <p class="sub-header">
                     Add <code>.table-hover</code> to enable a hover state on table rows within a
