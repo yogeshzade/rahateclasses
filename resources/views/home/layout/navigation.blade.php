f68b2fe0da5d024e6a428d3690df956a266b64eb
Shantanu Kulkarni <heyshantu13@gmail.com>
2021-03-22 12:21:24 +0530

    Change code

--- resources/views/home/layout/navigation.blade.php
+++ resources/views/home/layout/navigation.blade.php
@@ -35,7 +35,30 @@
                                   <li><a href=""></a></li>
                             </ul>
                         </li>
-                         <li> <a href="{{route('shownotes.index')}}">Notes</a></li>
+
+                        <li class="dropdown"> <a data-toggle="dropdown" href="">Pre Foundation Course <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{route('howtoapply.index')}}">#</a></li>
+                                  <li><a href=""></a></li>
+                            </ul>
+                        </li>
+
+                         <li class="dropdown"> <a data-toggle="dropdown" href="">IIT<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="">Quick Revision</a></li>
+                                  <li><a href="">IIT JEE Videos Lectures</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="dropdown"> <a data-toggle="dropdown" href="">Solutions<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="">H.C Verma</a></li>
+                                  <li><a href="">I.E Irodov</a></li>
+                                  <li><a href="">S.S Krotov</a></li>
+                            </ul>
+                        </li>
+
+                         <!-- <li> <a href="#">Notes</a></li> -->
                         <li> <a href="/career">Career</a></li>
                            <li> <a href="{{route('home.faculties')}}#ulfaculties">Faculties</a></li>
                             <li> <a href="/contact">Contact Us</a></li>
