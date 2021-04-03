f18181e298e067a0e4495f3ac6a745aa057f203d
Shantanu Kulkarni <heyshantu13@gmail.com>
2021-04-03 08:50:56 +0530

    update payment

--- resources/views/home/layout/navigation.blade.php
+++ resources/views/home/layout/navigation.blade.php
@@ -26,18 +26,6 @@
                                     <li><a href="#">10th Std</a></li>
                                 </ul>
                                </li>
-
-                                <li class="dropdown-submenu">
-                                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <span class="nav-label">Pre Foundation </span><span class="caret"></span></a>
-                                  <ul class="dropdown-menu">
-                                    <li><a href="#">7th Std</a></li>
-                                    <li><a href="#">8th Std</a></li>
-                                    <li><a href="#">9th Std</a></li>
-                                    <li><a href="#">10th Std</a></li>
-                                </ul>
-                               </li>
-
-
                               <!-- Dropdown End -->
                              </ul> 
                         </li>
