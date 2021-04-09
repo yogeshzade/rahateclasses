bc0cb1cd81d44fd10874902057fb9e567dc6fdc9
Shantanu Kulkarni <heyshantu13@gmail.com>
2021-04-09 10:07:40 +0530

    Update medical

--- resources/views/home/medical.blade.php
+++ resources/views/home/medical.blade.php
@@ -13,7 +13,12 @@
        @include('home.layout.navigation')
         <!-- End Navigation -->
         <style type="text/css">
-    p{color:#010101;font-size: 16px;}
+    p{ color: #010101;
+          }
+          span{
+            color:#010101;
+            line-height: 1.6;
+          }
         </style>
     </header>
 
@@ -34,98 +39,78 @@
 
                 <div class="col-md-12">
 
-                    <h3><strong>NEET</strong></h3>
-
-<p>The National eligibility and entrance test (NEET) is a National level exam held for MBBS and BDS. In 2020 around 16 Lakh students applied and in 2019 around 15 Lakh so, with that you can imagine how much the competition is? This is the reason we help our students for preparing for NEET in all the possible way -</p>
-
-<p>1. Test Series </p>
-
-<p>2. Revision </p>
-
-<p>3. Doubt clearing session</p>
-
-<p>4. Our Student Counseling and Grievances Cell </p>
-
-<p>This is the reason, Rahate's IIT and Medical Home is the Best NEET classes in Nagpur for 15 years and the journey continues to be the best and provide Quality Education to our Students.</p>
-
-<p>Today, also our students who were aspirants and now Doctors said, Our help in making a study plan, teaching and Guidance really helps them in their journey. We know preparing for entrance exams like NEET is not easy there are so much pressure and expectations so, in this, it's really important to guide them not as a teacher but sometimes as a friend and this is what our faculties do. </p>
-
-<p>NEET 2020</p>
-
-<table border="1" cellspacing="0" cellpadding="0" >
-<tbody>
-<tr>
-<td valign="top" ><p>Exam Name </p></td>
-<td valign="top" ><p>National eligibility and entrance test (NEET)</p></td>
-</tr>
-
-<tr>
-<td valign="top" ><p>Exam Frequency </p></td>
-<td valign="top" ><p>Once a year</p></td>
-</tr>
-
-<tr>
-<td valign="top" ><p>Exam Duration</p></td>
-<td valign="top" ><p>3 Hours</p></td>
-</tr>
-
-<tr>
-<td valign="top" ><p>Official Website</p></td>
-<td valign="top" ><p><a href="https://ntaneet.nic.in/Ntaneet/Welcome.aspx">https://ntaneet.nic.in/Ntaneet/Welcome.aspx</a> </p></td>
-</tr>
-
-</tbody>
+                  <p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;text-align:center;'><strong><span style="font-size:28px;line-height:115%;color:#980000;">NEET</span></strong></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>The National eligibility and entrance test (NEET) is a National level exam held for MBBS and BDS. In 2020 around 16 Lakh students applied and in 2019 around 15 Lakh so, with that you can imagine how much the competition is? This is the reason we help our students for preparing for NEET in all the possible way -</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>1. Test Series&nbsp;</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>2. Revision&nbsp;</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>3. Doubt clearing session</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>4. Our Student Counseling and Grievances Cell&nbsp;</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>&nbsp;</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>This is the reason, Rahate&apos;s IIT and Medical Home is the Best NEET classes in Nagpur for 15 years and the journey continues to be the best and provide Quality Education to our Students.</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>Today, also our students who were aspirants and now Doctors said, Our help in making a study plan, teaching and Guidance really helps them in their journey. We know preparing for entrance exams like NEET is not easy there are so much pressure and expectations so, in this, it&apos;s really important to guide them not as a teacher but sometimes as a friend and this is what our faculties do.&nbsp;</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>&nbsp;</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>NEET 2020</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>&nbsp;</p>
+<table style="width: 4.5e+2pt;border-collapse:collapse;border:none;">
+    <tbody>
+        <tr>
+            <td style="width: 225.7pt;border: 1pt solid black;padding: 5pt;vertical-align: top;">
+                <p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>Exam Name&nbsp;</p>
+            </td>
+            <td style="width: 225.7pt;border-top: 1pt solid black;border-right: 1pt solid black;border-bottom: 1pt solid black;border-image: initial;border-left: none;padding: 5pt;vertical-align: top;">
+                <p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>National eligibility and entrance test (NEET)</p>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 225.7pt;border-right: 1pt solid black;border-bottom: 1pt solid black;border-left: 1pt solid black;border-image: initial;border-top: none;padding: 5pt;vertical-align: top;">
+                <p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>Exam Frequency&nbsp;</p>
+            </td>
+            <td style="width: 225.7pt;border-top: none;border-left: none;border-bottom: 1pt solid black;border-right: 1pt solid black;padding: 5pt;vertical-align: top;">
+                <p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>Once a year</p>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 225.7pt;border-right: 1pt solid black;border-bottom: 1pt solid black;border-left: 1pt solid black;border-image: initial;border-top: none;padding: 5pt;vertical-align: top;">
+                <p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>Exam Duration</p>
+            </td>
+            <td style="width: 225.7pt;border-top: none;border-left: none;border-bottom: 1pt solid black;border-right: 1pt solid black;padding: 5pt;vertical-align: top;">
+                <p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>3 Hours</p>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 225.7pt;border-right: 1pt solid black;border-bottom: 1pt solid black;border-left: 1pt solid black;border-image: initial;border-top: none;padding: 5pt;vertical-align: top;">
+                <p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>Official Website</p>
+            </td>
+            <td style="width: 225.7pt;border-top: none;border-left: none;border-bottom: 1pt solid black;border-right: 1pt solid black;padding: 5pt;vertical-align: top;">
+                <p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><a href="https://ntaneet.nic.in/Ntaneet/Welcome.aspx"><span style="color:#1155CC;">https://ntaneet.nic.in/Ntaneet/Welcome.aspx</span></a>&nbsp;</p>
+            </td>
+        </tr>
+    </tbody>
 </table>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>&nbsp;</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>&nbsp; &nbsp;&nbsp;</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><strong>Frequently Asked Questions (FAQs):</strong></p>
+<div style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>
+    <ol style="margin-bottom:0cm;list-style-type: decimal;margin-left:0cmundefined;">
+        <li style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>Will NEET 2021 be conducted twice a year?</li>
+    </ol>
+</div>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>&nbsp; &nbsp; &nbsp;Yes, NEET exam will now be conducted twice.</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>&nbsp;</p>
+<div style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>
+    <ol style="margin-bottom:0cm;list-style-type: undefined;margin-left:0cmundefined;">
+        <li style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>How many times can I appear for NEET?</li>
+    </ol>
+</div>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>&nbsp; &nbsp; &nbsp;There is no limitation. You can appear any number of time.</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>&nbsp;</p>
+<div style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>
+    <ol style="margin-bottom:0cm;list-style-type: undefined;margin-left:0cmundefined;">
+        <li style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>Is the NEET 2021 exam online or offline?</li>
+    </ol>
+</div>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>&nbsp; &nbsp; NEET 2021 will be conduct on Offline mode.&nbsp;</p>
 
-<p>Frequently Asked Questions (FAQs):</p>
-
-<p>1. Will NEET 2021 be conducted twice a year?</p>
-
-<p>Yes, NEET exam will now be conducted twice.</p>
-
-<p>2. How many times can I appear for NEET?</p>
-
-<p>There is no limitation. You can appear any number of time.</p>
-
-<p>3. Is the NEET 2021 exam online or offline?</p>
-
-<p>NEET 2021 will be conduct on Offline mode. </p>
-
-                   
-                </div>
-<br/>
-                <div class="col-md-12">
-                    <h3><strong>JIPMER</strong></h3>
-
-<p>The aim of most medical aspirants is to get admission to the JIPMER MBBS. JIPMER (Jawaharlal Institute of Postgraduate Medical Education and Research, Puducherry.) comes at the top in the list of Best Medical institutions in India. We Rahate's IIT and Medical Home is the best JIPMER coaching institute in Nagpur. </p>
-
-<p>Why you should take admission? </p>
-
-<p>* Experienced and Core Faculties for each subject</p>
-
-<p>* Scientifically Designed Course</p>
-
-<p>* Library Facility</p>
-
-<p>* Reading Room</p>
-
-<p>* Student Counseling and Grievances Cell</p>
-
-<p>* Full-time Faculty Assistance</p>
-
-<p>* Positive Mental attitudes are taught</p>
-
-<p>* Short and Fast study techniques are taught </p>
-
-<p>Frequently Asked Questions (FAQs):</p>
-
-<p>1. How can I crack the JIPMER 2021?</p>
-
-<p>2. Which books are best for JIPMER?</p>
-
-<p>3. What is the best way to prepare for the JIPMER and the AIIMS?</p>
-
-<p>4. Does JIPMER have negative marking?</p>
 
                 </div>
 
