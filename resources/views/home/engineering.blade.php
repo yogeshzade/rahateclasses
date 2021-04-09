ab552415745fa4d9340b72bcf6eb0bd5bdb4b3bb
Shantanu Kulkarni <heyshantu13@gmail.com>
2021-04-09 10:03:47 +0530

    Update engineering page

--- resources/views/home/engineering.blade.php
+++ resources/views/home/engineering.blade.php
@@ -32,129 +32,49 @@
       
         <div class="row">
 
-            <div class="col-md-12">
-                <h3> IIT - JEE</h3>
-                <br/>
-
-                <p>
-    We can proudly say, Rahate's IIT and Medical Home is the Best IIT JEE
-    Coaching in Nagpur
-</p>
-<p>
-    It's not just a word for us rather it's our ambition to provide the best
-    Theoretical and Practical Knowledge to our Students and so, with this aim,
-    our number of students got into IITs and NITs.
-</p>
-<p>
-    When the student who was aspirants of IIT came to us, our pattern for
-    preparation will be -
-</p>
-<p>
-    1. Test Series - We took the first test on a chapter basis like immediately
-    when one chapter is done. then, on 2 - 3 Chapters and so on finally when
-    all syllabus covers then, continues doubt clearing session, Revision, and
-    more and more Tests.
-</p>
-<p>
-    2. Study Planning - Our experts help students to make topic wise study
-    timetable (particularly for each student) and make sure that students
-    following it.
-</p>
-<p>
-    In that time it’s very important to have the right guidance from the right
-    people, this is what exactly we do.
-</p>
-<p>
-    Our students who are working now in Multinational companies, doing
-    business, or studying in IIT's and NIT's told us, our efforts, guidance
-    really helped them and because of this Rahate's IIT and Medical Home is the
-    best IIT JEE coaching in Nagpur.
-</p>
-<p>
-    Frequently Asked Questions (FAQs):
-</p>
-<p>
-    1. Can I get extra doubt removal sessions for clearing my Doubts?
-</p>
-<p>
-    Yes, We do take extra classes to solve the doubts of students.
-</p>
-<p>
-    2. Do you take the test series and when?
-</p>
-<p>
-    Yes, we do take test series every week.
-</p>
-<p>
-    3. Can I pay the Fee in Installments?
-</p>
-<p>
-    Yes, you can pay fees in installments (Official fee chart).
-</p>
-<p>
-    4. What kinds of Study Materials would be provided?
-</p>
-<p>
-    We provide Printed study material like DPP’s (Daily Practice Papers),
-    Assignments, Notes.
-</p>
-
-
+           <div class="col-lg-12">
+            <p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;text-align:center;'><strong><span style="font-size:28px;line-height:115%;color:#980000;">IIT - JEE</span></strong></p>
+             <br/>
+                <img src="/iitjee.jpeg" class="img-responsive center-block d-block mx-auto" alt="Sample Image" style="
+    width: 550px;
+">
+                
             </div>
-            <br/>
 
-            <hr/>
-            <br/>
             <div class="col-md-12">
-                <h3> MH-CET</h3>
-                <br/>
-
-                <p>
-    Are you preparing for MH-CET? Then, you should start with understanding the
-    syllabus, also, you must be in doubt about how can I study effectively so
-    that I will get top rank right?
-</p>
-<p>
-    For Competitive exams like MH-CET, you have to take guidance from experts.
-</p>
-<p>
-    We Rahate’s IIT and Medical Home have those experts for you!
-</p>
-<p>
-    Our experts make sure that each and every student of the class understand
-    the topics which were taught from basics.
-</p>
-<p>
-    Our Student Counseling and Grievances cell present all the time for you!
-    And with that we can say, proudly we Rahate’s IIT and Medical Home is the
-    best MH-CET coaching classes in Nagpur.
-</p>
-<p>
-    Frequently Asked Questions (FAQ)
-</p>
-<p>
-    1. What is the syllabus of the MH-CET exam?
-</p>
-<p>
-    Please refer (Our website link of the NATA syllabus).
-</p>
-<p>
-    2. Is the MH-CET exam conduct online/offline?
-</p>
-<p>
-    <strong> </strong>
-    <strong> </strong>
-    MH-CET conducted in the online mode. <strong> </strong>
-</p>
               
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'>&nbsp;</p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">We can proudly say, Rahate&apos;s IIT and Medical Home is the Best IIT JEE Coaching in Nagpur</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">It&apos;s not just a word for us rather it&apos;s our ambition to provide the best Theoretical and Practical Knowledge to our Students and so, with this aim, our number of students got into IITs and NITs.</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">&nbsp;</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">When the student who was aspirants of IIT came to us,&nbsp;our pattern for preparation will be&nbsp;-&nbsp;</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">&nbsp;</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">1. Test Series - We took the first test on a chapter basis like immediately when one chapter is done. then, on 2 - 3 Chapters and so on finally when all syllabus covers then, continues doubt clearing session, Revision, and more and more Tests.</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">2. Study Planning - Our experts help students to make topic wise study timetable (particularly for each student) and make sure that students following it. &nbsp;</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">&nbsp;</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">&nbsp;In that time it&rsquo;s very important to have the right guidance from the right people, this is what exactly we do.</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">&nbsp;</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">Our students who are working now in Multinational companies, doing business, or studying in IIT&apos;s and NIT&apos;s told us, our efforts, guidance really helped them and because of this Rahate&apos;s IIT and Medical Home is the best IIT JEE coaching in Nagpur. &nbsp;&nbsp;</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">&nbsp;</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;"><strong>Frequently Asked Questions (FAQs):</strong></span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><br></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">1. Can I get extra doubt removal sessions for clearing my Doubts?</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">Yes, We do take extra classes to solve the doubts of students.</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">&nbsp;</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">2. Do you take the test series and when?</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">Yes, we do take test series every week.</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">&nbsp;</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">3. Can I pay the Fee in Installments?</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">Yes, you can pay fees in installments (Official fee chart).</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">&nbsp;</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><span style="font-size: 17px;">4. What kinds of Study Materials would be provided?</span></p>
+<p style='margin:0cm;line-height:115%;font-size:15px;font-family:"Arial",sans-serif;'><br></p>
+<p style="margin: 0cm; line-height: 1.5; font-size: 15px; font-family: Arial, sans-serif;"><span style="font-size: 17px;"><strong>We provide Printed study material like DPP&rsquo;s (Daily Practice Papers), Assignments, Notes.</strong></span></p>
             </div>
-
-            <br/>
-            <hr/>
 
         </div>
 
-    </div>
+      </div>
   
 
 </section>
